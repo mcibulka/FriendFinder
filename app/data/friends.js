277b6e0ef4e06a56a35f51e07db07e17b3b1f870
Matthew Cibulka <mcibulkadev@gmail.com>
2018-09-15 19:42:03 -0400

    Added friends data.

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+const friends = [{
+  "name": "Emma",
+  "photo": "https://photo.url.com/profile_photo.jpg",
+  "scores": [1, 3, 3, 4, 5, 5, 2, 3, 4, 2]
+},
+{
+  "name": "Olivia",
+  "photo": "https://photo.url.com/profile_photo.jpg",
+  "scores": [5, 5, 4, 4, 1, 1, 3, 5, 2, 1]
+},
+{
+  "name": "Sophia",
+  "photo": "https://photo.url.com/profile_photo.jpg",
+  "scores": [1, 4, 2, 5, 5, 5, 1, 3, 2, 1]
+},
+{
+  "name": "Isabella",
+  "photo": "https://photo.url.com/profile_photo.jpg",
+  "scores": [2, 2, 3, 2, 2, 1, 1, 5, 4, 3]
+},
+{
+  "name": "Kayla",
+  "photo": "https://photo.url.com/profile_photo.jpg",
+  "scores": [3, 4, 2, 5, 1, 5, 2, 3, 2, 5]
+},
+{
+  "name": "Liam",
+  "photo": "https://photo.url.com/profile_photo.jpg",
+  "scores": [3, 2, 5, 5, 5, 1, 1, 5, 2, 4]
+},
+{
+  "name": "Noah",
+  "photo": "https://photo.url.com/profile_photo.jpg",
+  "scores": [2, 3, 3, 4, 1, 5, 3, 5, 4, 1]
+},
+{
+  "name": "William",
+  "photo": "https://photo.url.com/profile_photo.jpg",
+  "scores": [4, 2, 3, 1, 3, 3, 5, 2, 1, 2]
+},
+{
+  "name": "James",
+  "photo": "https://photo.url.com/profile_photo.jpg",
+  "scores": [4, 5, 4, 3, 2, 4, 2, 3, 1, 4]
+},
+{
+  "name": "Oliver",
+  "photo": "https://photo.url.com/profile_photo.jpg",
+  "scores": [5, 2, 1, 2, 2, 4, 5, 2, 3, 2]
+}
+];
