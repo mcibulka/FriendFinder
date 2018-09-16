78c2caa7b118f19e34263fd1b1d1a18a976350d9
Matthew Cibulka <mcibulkadev@gmail.com>
2018-09-16 14:28:23 -0400

    Added /api/friends POST route with friend match logic.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,37 @@ module.exports = (app) => {
   app.get("/api/friends", (req, res) => {
     res.json(friendsList);
   });
+
+
+  app.post("/api/friends", (req, res) => {
+    let lowest = 0;
+    let match = "";
+
+    friendsList.forEach((friend) => {
+      let totDiff = 0;
+      
+      // Calculate total difference between each friend
+      for (let i = 0 ; i < 10 ; i++) {
+        const userScore = req.body.scores[i];
+        const friendScore = friend.scores[i];
+
+        const diff = Math.abs(userScore - friendScore);
+        totDiff += diff;
+      }
+
+      // Set default match to first friend, Emma
+      if (friend.name === "Emma") {
+        lowest = totDiff;
+        match = "Emma";
+      }
+      else {
+        if (totDiff < lowest) {
+          lowest = totDiff;
+          match = friend.name;
+        }
+      }
+    });
+
+    res.send(`<h1>Match</h1><h2>${match}</h2>`);
+  });
 };
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
