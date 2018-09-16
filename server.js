4f00f548370fcc46601418edc67a3620aa2a3173
Matthew Cibulka <mcibulkadev@gmail.com>
2018-09-16 11:58:31 -0400

    Restructured routing to routes folder.

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
 
-var app = express();
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000);
+const app = express();
+
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(3000, () => {
+  console.log("App running on port 3000!");
+});
